4aefd111e78d7f78f321db3b5ce6fa1f44bbabe8
erhvict <64385074+andrei1733@users.noreply.github.com>
2024-02-05 02:27:42 +0500

    Added a pizza page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import NotFound from './pages/404';
 import Cart from './pages/Cart';
+import FullPizza from './pages/FullPizza';
 import Home from './pages/Home';
 import './scss/app.scss';
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/pizza/:id" element={<FullPizza />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
