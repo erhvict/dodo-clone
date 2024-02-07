8178d9832b8cfdb69de2fb2c338ab08199672415
erhvict <64385074+andrei1733@users.noreply.github.com>
2024-02-08 03:53:47 +0500

    Full Pizza page migrated to TS, typed

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -1,9 +1,15 @@
 import axios from 'axios';
+import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
-const FullPizza = () => {
-  const [pizza, setPizza] = useState();
+const FullPizza: React.FC = () => {
+  const [pizza, setPizza] = useState<{
+    imageUrl: string;
+    title: string;
+    price: number;
+  }>();
+
   const { id } = useParams();
   const navigate = useNavigate();
 
