0754d39bb1b962c909398ac03af2847e27243bae
erhvict <64385074+andrei1733@users.noreply.github.com>
2024-02-10 05:39:28 +0500

    Add: saving cart to localStorage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,17 @@ import { Search } from './Search';
 
 const Header: React.FC = () => {
   const { items, totalPrice } = useSelector(selectCart);
+  const isMounted = React.useRef(false);
 
   const totalCount = items.reduce((sum: number, item: any) => sum + item.count, 0);
+
+  React.useEffect(() => {
+    if (isMounted.current) {
+      const json = JSON.stringify(items);
+      localStorage.setItem('cart', json);
+    }
+    isMounted.current = true;
+  }, [items]);
 
   return (
     <div className="header">
--- src/redux/slices/cartSlice.ts
+++ src/redux/slices/cartSlice.ts
@@ -1,4 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { getCartFormLS } from '../../utils/getCartFromLS';
+import { calcTotalPrice } from './../../utils/calcTotalPrice';
 import { RootState } from './../store';
 
 export type CartItem = {
@@ -16,9 +18,11 @@ interface CartSliceState {
   items: CartItem[];
 }
 
+const { items, totalPrice } = getCartFormLS();
+
 const initialState: CartSliceState = {
-  totalPrice: 0,
-  items: [],
+  totalPrice,
+  items,
 };
 
 const cartSlice = createSlice({
@@ -36,9 +40,7 @@ const cartSlice = createSlice({
         });
       }
 
-      state.totalPrice = state.items.reduce((sum, obj) => {
-        return obj.price * obj.count + sum;
-      }, 0);
+      state.totalPrice = calcTotalPrice(state.items);
     },
     minusItem(state, action: PayloadAction<string>) {
       const findItem = state.items.find((obj) => obj.id === action.payload);
